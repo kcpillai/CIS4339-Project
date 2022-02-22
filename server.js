e2ab53004faf9f81a71df4cb85d3d50a59ea8cff
kcpillai <77808972+kcpillai@users.noreply.github.com>
2022-02-21 18:37:08 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,43 @@
+const http = require("http");
+const express = require("express");
+const mongoose = require("mongoose");
+
+// Connection string for MongoDB
+const MONGO_URL =
+  "mongodb+srv://group14:cis4339@projectcluster.kd8kw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+mongoose.connect(MONGO_URL);
+
+const app = express();
+
+const PORT = 3000;
+const server = http.createServer(app);
+
+// Testing if connection works
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+// logging if there is an error connecting to the MongoDB
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
+// Middleware
+app.use((req, res, next) => {
+  const start = Date.now();
+  next();
+  const delta = Date.now() - start;
+  console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
+});
+
+//Connecting to MongoDB
+async function startServer() {
+  await mongoose.connect(MONGO_URL);
+
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
+}
+
+startServer();
