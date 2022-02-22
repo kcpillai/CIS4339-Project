a7266ec6f78e01002bfc624f28db6dcee2e7b194
kcpillai <Kpillai23@gmail.com>
2022-02-21 21:40:20 -0600

    Health Model Fix

--- models/health.models.js
+++ models/health.models.js
@@ -3,33 +3,33 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 let healthSchema = new Schema({
-    _id: {
-        type: String,
-        default: uuid.v1
-    },
-    clientID: {
-		type: String,
-      	required: true
-	},
-    hasHealthInsurance: {
-        type: Boolean,
-        required: true
-    },
-    healthInsuranceProgram: {
-        type: String
-    },
-    hasFoodStamps: {
-        type: Boolean,
-        required: true
-    },
-    foodStampsAmount: {
-        type: Number,
-    },
-    foodStampsReason: {
-        type: String
-        },
-    {
-        collection: 'health'
-    });
+  _id: {
+    type: String,
+    default: uuid.v1,
+  },
+  clientID: {
+    type: String,
+    required: true,
+  },
+  hasHealthInsurance: {
+    type: Boolean,
+    required: true,
+  },
+  healthInsuranceProgram: {
+    type: String,
+  },
+  hasFoodStamps: {
+    type: Boolean,
+    required: true,
+  },
+  foodStampsAmount: {
+    type: Number,
+  },
+  foodStampsReason: {
+    type: String,
+  },
 
-module.exports = mongoose.model('health', healthSchema);
+  collection: "health",
+});
+
+module.exports = mongoose.model("health", healthSchema);
