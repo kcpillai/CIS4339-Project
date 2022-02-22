2dcda06a1f93f0cb44b34bb70e29818c8b977d34
hchen54 <70294661+hchen54@users.noreply.github.com>
2022-02-21 21:19:01 -0600

    julia's models commit 1

---
+++ models/health.models.js
@@ -0,0 +1,35 @@
+const uuid = require("uuid");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+let healthSchema = new Schema({
+    _id: {
+        type: String,
+        default: uuid.v1
+    },
+    clientID: {
+		type: String,
+      	required: true
+	},
+    hasHealthInsurance: {
+        type: Boolean,
+        required: true
+    },
+    healthInsuranceProgram: {
+        type: String
+    },
+    hasFoodStamps: {
+        type: Boolean,
+        required: true
+    },
+    foodStampsAmount: {
+        type: Number,
+    },
+    foodStampsReason: {
+        type: String
+}
+    {
+        collection: 'health'
+    });
+
+module.exports = mongoose.model('health', healthSchema);
