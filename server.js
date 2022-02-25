907eeae2c5706ea9c6aa76c447d366f7f9183fad
kcpillai <Kpillai23@gmail.com>
2022-02-24 20:20:07 -0600

    updates

--- server.js
+++ server.js
@@ -41,197 +41,23 @@ app.use((req, res, next) => {
 });
 
 // Setting up routers
-// const employeeRouter = require('./routes/employee.router.js');
-// app.use('/employees', employeeRouter);
+const employeeRouter = require('./routes/employee.router.js');
+app.use('/employees', employeeRouter);
 
-// const eventsRouter = require('./routes/events.router.js');
-// app.use('/events', eventsRouter);
+const eventsRouter = require('./routes/events.router.js');
+app.use('/events', eventsRouter);
 
-// const healthRouter = require('./routes/health.router.js');
-// const employeesModel = require('./models/employees.model.js');
-// app.use('/health', healthRouter);
+const healthRouter = require('./routes/health.router.js');
+app.use('health', healthRouter);
 
-// const familiesRouter = require("./routes/families.router.js");
-// app.use("/families", familiesRouter);
+const familiesRouter = require('./routes/families.router.js');
+app.use('/families', familiesRouter);
 
-// const educationRouter = require("./routes/employees.router.js");
-// app.use("/education", educationRouter);
+const educationRouter = require('./routes/employees.router.js');
+app.use('/education', educationRouter);
 
-// const clientsRouter = require("./routes/clients.router.js");
-// app.use("/clients", clientsRouter);
+const clientsRouter = require('./routes/clients.router.js');
+app.use('/clients', clientsRouter);
 
-const employeeModel = require('./models/employees.model.js');
-
-// GET Employees
-app.get('/employees', (req, res, next) => {
-  //very plain way to get all the data from the collection through the mongoose schema
-  employeeModel.find((error, data) => {
-    if (error) {
-      //here we are using a call to next() to send an error message back
-      return next(error);
-    } else {
-      res.json(data);
-    }
-  });
-});
-// ADD Employees
-app.post('/employees', (req, res, next) => {
-  employeeModel.create(req.body, (error, data) => {
-    if (error) {
-      return next(error);
-    } else {
-      res.send('Employee information is added to the database.');
-    }
-  });
-});
-// DELETE Employees
-app.delete('/employees/:id', (req, res, next) => {
-  //mongoose will use studentID of document
-  employeeModel.findOneAndRemove(
-    { employeeId: req.params.id },
-    (error, data) => {
-      if (error) {
-        return next(error);
-      } else {
-        res.status(200).json({
-          msg: data,
-        });
-        res.send('Employee has been deleted');
-      }
-    }
-  );
-});
-// Updating Employee
-app.put('/employees/:id', (req, res, next) => {
-  employeeModel.findOneAndUpdate(
-    { employeeId: req.params.id },
-    {
-      $set: req.body,
-    },
-    (error, data) => {
-      if (error) {
-        return next(error);
-      } else {
-        res.send('Employee is edited via PUT');
-        console.log('Employee has been successfully updated!', data);
-      }
-    }
-  );
-});
-
-// Events Collection
-const eventsModel = require('./models/employees.model.js');
-
-// GET events
-app.get('/events', (req, res, next) => {
-  //very plain way to get all the data from the collection through the mongoose schema
-  eventsModel.find((error, data) => {
-    if (error) {
-      //here we are using a call to next() to send an error message back
-      return next(error);
-    } else {
-      res.json(data);
-    }
-  });
-});
-// ADD Events
-app.post('/events', (req, res, next) => {
-  eventsModel.create(req.body, (error, data) => {
-    if (error) {
-      return next(error);
-    } else {
-      res.send('Events information is added to the database.');
-    }
-  });
-});
-// DELETE Events
-app.delete('/events/:id', (req, res, next) => {
-  //mongoose will use studentID of document
-  eventsModel.findOneAndRemove({ employeeId: req.params.id }, (error, data) => {
-    if (error) {
-      return next(error);
-    } else {
-      res.status(200).json({
-        msg: data,
-      });
-      res.send('Employee has been deleted');
-    }
-  });
-});
-// Updating Events
-app.put('/events/:id', (req, res, next) => {
-  eventsModel.findOneAndUpdate(
-    { eventsId: req.params.id },
-    {
-      $set: req.body,
-    },
-    (error, data) => {
-      if (error) {
-        return next(error);
-      } else {
-        res.send('Employee is edited via PUT');
-        console.log('Employee has been successfully updated!', data);
-      }
-    }
-  );
-});
-
-// Organizations
-const organizationsModel = require('./models/employees.model.js');
-
-// GET organizations
-app.get('/organizations', (req, res, next) => {
-  //very plain way to get all the data from the collection through the mongoose schema
-  organizationsModel.find((error, data) => {
-    if (error) {
-      //here we are using a call to next() to send an error message back
-      return next(error);
-    } else {
-      res.json(data);
-    }
-  });
-});
-// ADD organizations
-app.post('/organizations', (req, res, next) => {
-  organizationsModel.create(req.body, (error, data) => {
-    if (error) {
-      return next(error);
-    } else {
-      res.send('organizations information is added to the database.');
-    }
-  });
-});
-// DELETE organizations
-app.delete('/organizations/:id', (req, res, next) => {
-  //mongoose will use studentID of document
-  organizationsModel.findOneAndRemove(
-    { organizationsId: req.params.id },
-    (error, data) => {
-      if (error) {
-        return next(error);
-      } else {
-        res.status(200).json({
-          msg: data,
-        });
-        res.send('organization has been deleted');
-      }
-    }
-  );
-});
-// Updating organizations
-app.put('/organizations/:id', (req, res, next) => {
-  eventsModel.findOneAndUpdate(
-    { eventsId: req.params.id },
-    {
-      $set: req.body,
-    },
-    (error, data) => {
-      if (error) {
-        return next(error);
-      } else {
-        res.send('organization is edited via PUT');
-        console.log('organization has been successfully updated!', data);
-      }
-    }
-  );
-});
+const organziationsRouter = require('./routes/organizations.router.js');
+app.use('/organizations', organziationsRouter);
