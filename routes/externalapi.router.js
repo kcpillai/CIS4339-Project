da7aaabcaee3b39a726610917561c0708db0041f
kcpillai <Kpillai23@gmail.com>
2022-03-02 21:11:18 -0600

    External API Fix

--- routes/externalapi.router.js
+++ routes/externalapi.router.js
@@ -3,24 +3,28 @@ const express = require('express');
 const axios = require('axios');
 
 // get all data
-axios.get('https://cis-4339.herokuapp.com/api/v1/data')
-    .then(response => {
-        console.log(response.data.status);
-        // console.log(response.data);
-        res.send(response.data.status);
-    })
-    .catch(error => {
-        console.log(error);
-    });
-
-    // get data from specific client
-    
-axios.get('https://cis-4339.herokuapp.com/api/v1/data/{{first_name}}/{{last_name}}/{{phone_number}}')
-.then(response => {
+axios
+  .get('https://cis-4339.herokuapp.com/api/v1/data')
+  .then(response => {
     console.log(response.data.status);
     // console.log(response.data);
     res.send(response.data.status);
-})
-.catch(error => {
+  })
+  .catch(error => {
     console.log(error);
-});
+  });
+
+// get data from specific client
+
+axios
+  .get(
+    'https://cis-4339.herokuapp.com/api/v1/data/{{first_name}}/{{last_name}}/{{phone_number}}'
+  )
+  .then(response => {
+    console.log(response.data.status);
+    // console.log(response.data);
+    res.send(response.data.status);
+  })
+  .catch(error => {
+    console.log(error);
+  });
